af4722adc8c4d0dabfbea39ea0b2d10350619ebb
N3xTr1Me <destriker17@gmail.com>
2022-07-05 23:20:48 +0300

    Command adjustments

--- evpy/commands/genetic_operators/wrappers/command.py
+++ evpy/commands/genetic_operators/wrappers/command.py
@@ -1,20 +1,32 @@
+# factory method for Command
+def make_com(function):
+    return Command(function)
+
+
 # command module to wrap genetic operators
-
-
 class Command(object):
     def __init__(self, function):
         self.function = function
 
-    def __call__(self, *args):
+    def __call__(self, **kwargs):
+        # (!) as of now parsing as positional arguments, need to figure out how to parse as named in case of abstract
+        # function
 
-        if 2 <= len(args) < 3:
-            par_1, par_2 = args[0], args[1]
+        if len(kwargs.keys()) == 1:
+            par_1 = list(kwargs.values())[0]
+            return self.function(par_1)
+
+        elif 2 <= len(kwargs) < 3:
+            parameters = list(kwargs.values())
+            par_1, par_2 = parameters[0], parameters[1]
             return self.function(par_1, par_2)
 
-        elif 3 <= len(args) < 4:
-            par_1, par_2, par_3 = args[0], args[1], args[2]
+        elif 3 <= len(kwargs) < 4:
+            parameters = list(kwargs.values())
+            par_1, par_2, par_3 = parameters[0], parameters[1], parameters[2]
             return self.function(par_1, par_2, par_3)
 
         else:
-            par_1, par_2, par_3, par_4 = args[0], args[1], args[2], args[3]
+            parameters = list(kwargs.values())
+            par_1, par_2, par_3, par_4 = parameters[0], parameters[1], parameters[2], parameters[3]
             return self.function(par_1, par_2, par_3, par_4)
