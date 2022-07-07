dc40b02b591702d37dd127c43b4d6d4c31f7af58
N3xTr1Me <destriker17@gmail.com>
2022-07-08 02:00:43 +0300

    Added algorithm base class and it's factory

---
+++ evpy/algorithms/base/algorithm_base.py
@@ -0,0 +1,16 @@
+from evpy.commands.genetic_operators.wrappers.facade.kernel import Kernel
+
+
+def make_algorithm(kernel: Kernel, fitness):
+    return AlgorithmBase(kernel, fitness)
+
+
+class AlgorithmBase:
+    def __init__(self, kernel, fitness):
+
+        self.__get_fitness = fitness
+        self.__kernel = kernel
+
+        self.__memory = []
+        self.__fittest = None
+        self.__max_fitness = None
---
+++ evpy/algorithms/base/base_factory.py
@@ -0,0 +1,23 @@
+from evpy.algorithms.base.algorithm_base import make_algorithm
+from evpy.commands.genetic_operators.wrappers.facade.kernel_factory import KernelFactory
+from evpy.commands.genetic_operators.wrappers.decorators.command_list import CommandList
+
+
+class BaseFactory:
+    def __init__(self, builder=make_algorithm, supported_commands=None):
+
+        if not supported_commands:
+            self.__supported = CommandList()
+        else:
+            self.__supported = supported_commands
+
+        self.__builder = builder
+        self.__supplier = KernelFactory()
+
+    def build_algorithm(self, fitness_function, algorithm: dict):
+        _kernel = self.__supplier.build_kernel(self.__supported.get_command(algorithm["mutation"]),
+                                               self.__supported.get_command(algorithm["recombination"]),
+                                               self.__supported.get_command(algorithm["population_selector"]),
+                                               self.__supported.get_command(algorithm["parent_selector"]))
+
+        return self.__builder(_kernel, fitness_function)
