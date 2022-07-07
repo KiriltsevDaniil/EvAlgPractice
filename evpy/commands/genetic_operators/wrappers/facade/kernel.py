fe658d51c1db3d6b6d3ee40a1d236a9b212bb83c
N3xTr1Me <destriker17@gmail.com>
2022-07-07 17:32:07 +0300

    Changed Facade initialization

--- evpy/commands/genetic_operators/wrappers/facade/kernel.py
+++ evpy/commands/genetic_operators/wrappers/facade/kernel.py
@@ -2,36 +2,12 @@ from evpy.commands.genetic_operators.wrappers.decorators.command_factory import 
 
 
 class Kernel:
-    def __init__(self, mutator=None, recombinator=None, pop_selector=None, parent_selector=None, random_choice=False):
+    def __init__(self, mutator=None, recombinator=None, pop_selector=None, parent_selector=None):
 
-        builder = CommandFactory()
-
-        if not random_choice:
-            if not mutator:
-                self.__mutator = None
-            else:
-                self.__mutator = builder.build_command("mutation", mutator)
-
-            if not recombinator:
-                self.__recombinator = None
-            else:
-                self.__recombinator = builder.build_command("recombination", recombinator)
-
-            if not pop_selector:
-                self.__pop_selector = None
-            else:
-                self.__pop_selector = builder.build_command("population_selector", pop_selector)
-
-            if not parent_selector:
-                self.__parent_selector = None
-            else:
-                self.__parent_selector = builder.build_command("parent_selector", parent_selector)
-
-        else:
-            self.__mutator = builder.build_command("mutation")
-            self.__recombinator = builder.build_command("recombination")
-            self.__pop_selector = builder.build_command("population_selector")
-            self.__parent_selector = builder.build_command("parent_selector")
+        self.__mutator = mutator
+        self.__recombinator = recombinator
+        self.__pop_selector = pop_selector
+        self.__parent_selector = parent_selector
 
     # (!!!) TO_DO: implement positional and non-positional argument input handling in the same function
     # for now it's only positional for Kernel. Command has a somewhat working prototype,
