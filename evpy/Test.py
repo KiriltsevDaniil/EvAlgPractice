20f9f2e54d9765b39137ab66fc5ded8c93e70c7c
N3xTr1Me <destriker17@gmail.com>
2022-07-08 20:51:53 +0300

    Removed OperatorKernel

--- evpy/Test.py
+++ evpy/Test.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt
-import algorithms.classic.Canonical as algorithm
-import algorithms.classic.Genitor as algorithm2
+from evpy.algorithms.classic.canonical_factory import CanonicalFactory
+from evpy.algorithms.classic.genitor_factory import GenitorFactory
 
-ga = algorithm.Canonical(lambda x: sum(x)+1, pop_size=10, g_len=10)
-ga.evaluate(T=4000, p_mut=.5, p_gene_mut=.5)
+ga_fac = CanonicalFactory()
+ga = ga_fac.build_canonical(fitness_function=lambda x: sum(x) + 1, pop_size=10, gen_len=10)
+
+gen_fac = GenitorFactory()
+genitor = gen_fac.build_genitor(fitness_function=lambda x: sum(x)+1, recombinator="single_point_crossover",
+                                mutator="point_mutation", pop_size=10, gen_len=10)
 
 
-genitor = algorithm2.Genitor(
-    lambda x: sum(x)+1, recombination="single_point_crossover", mutator="point_mutation", pop_size=10, g_len=10)
-genitor.evaluate(T=10000, p_mut=.5, p_gene_mut=.5)
-print(ga.fittest)
-print(genitor.fittest)
+print(ga.evaluate(T=4000, p_mut=.5, p_gene_mut=.5))
+print(genitor.evaluate(T=500, p_mut=.5, p_gene_mut=.5))
 
-x_coordinates2, y_coordinates2 = [x[1] for x in genitor.memory], [x[0] for x in genitor.memory]
-x_coordinates, y_coordinates = [x[1] for x in ga.memory], [x[0] for x in ga.memory]
+x_coordinates2, y_coordinates2 = [x[1] for x in genitor._get_memory()], [x[0] for x in genitor._get_memory()]
+x_coordinates, y_coordinates = [x[1] for x in ga._get_memory()], [x[0] for x in ga._get_memory()]
 plt.title("Canonical v.s. Genotor")
 plt.axhline(y=11, color='g', label='analytic bound')
 plt.grid()
